44028f97c80f620bf7aab6d0ddb2878d9b07194f
Mark Dube <mjdube99@gmail.com>
2021-05-27 23:27:48 -0500

    updated to allow for multiple stocks, separated by a comma

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import json
 import requests
-import time
 from stock import Stock
 
+comma = ","
+
+def printStock(uinput, stockpicked):
+    # Checks to see if the stock symbol exists (all prices will show as 0 if it doesn't)
+    # If it doesn't it will return the main function
+    # If the stock exists, it will print out the current price, daily high, daily low, and previous close
+    if stockpicked.current_price == 0:
+        print("\nUnknown stock, please try again.\n")
+        return main()
+    print(f"\n --- Stock information for {uinput} --- ")
+    print(f"Current Price for {uinput}: {stockpicked.current_price}")
+    print(f"Daily High for {uinput}: {stockpicked.daily_high}")
+    print(f"Daily low for {uinput}: {stockpicked.daily_low}")
+    print(f"Previous close for {uinput}: {stockpicked.previous_close}\n")
+
 def main():
+    uinput = str(input("What stock(s) would you like information for: "))
+    if comma in uinput:
+        uinput = uinput.upper()
+        multi_stock = uinput.split(",")
+        for x in range(len(multi_stock)):
+            stockpicked = Stock(multi_stock[x])
+            stockpicked.update_stock()
+            printStock(multi_stock[x], stockpicked)
+    else:
+        uinput = uinput.upper()
+        stockpicked = Stock(uinput)
+        stockpicked.update_stock()
 
-    uinput = str(input("What stock would you like information for: "))
-    stockpicked = Stock(uinput.upper())
-    stockpicked.update_stock()
-    print(f" --- Stock information for {uinput.upper()}--- ")
-    print(f"Current Price for {uinput.upper()}: {stockpicked.current_price}")
-    print(f"Daily High for {uinput.upper()}: {stockpicked.daily_high}")
-    print(f"Daily low for {uinput.upper()}: {stockpicked.daily_low}")
-
+        printStock(uinput, stockpicked)
 
 
 if __name__ == "__main__":
