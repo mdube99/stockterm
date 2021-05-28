f09f2a1980c775b5f313d16455a2332a46d3e013
Mark Dube <mjdube99@gmail.com>
2021-05-27 23:29:05 -0500

    changed to use f-strings

--- web_helper.py
+++ web_helper.py
@@ -6,10 +6,10 @@ API_KEY = readConfig.getData('web_helper')
 # print("Key: {}".format(API_KEY))
 
 def get_stock_price(symbol):
-    response = requests.get("https://finnhub.io/api/v1/quote?symbol={}&token={}".format(symbol, API_KEY))
+    response = requests.get(f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={API_KEY}")
     return json.loads(response.text)
 
 def get_crypto_price(symbol):
-    response = requests.get("https://finnhub.io/api/v1/crypto/symbol={}?exchange=binance&token={}".format(symbol, API_KEY))
+    response = requests.get(f"https://finnhub.io/api/v1/crypto/symbol={symbol}?exchange=binance&token={API_KEY}")
     print(response.text)
     return response
